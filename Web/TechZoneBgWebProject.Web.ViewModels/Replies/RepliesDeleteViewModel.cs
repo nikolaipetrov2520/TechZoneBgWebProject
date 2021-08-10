ecd44823d51d390fe38dac65ef74a9ad3ad01347
nikolaipetrov2520 <64483062+nikolaipetrov2520@users.noreply.github.com>
2021-08-10 15:07:03 +0300

    Fix some bugs

--- Web/TechZoneBgWebProject.Web.ViewModels/Replies/RepliesDeleteViewModel.cs
+++ Web/TechZoneBgWebProject.Web.ViewModels/Replies/RepliesDeleteViewModel.cs
@@ -23,15 +23,7 @@
 
         public int Likes { get; set; }
 
-        public int Loves { get; set; }
-
-        public int HahaCount { get; set; }
-
-        public int WowCount { get; set; }
-
-        public int SadCount { get; set; }
-
-        public int AngryCount { get; set; }
+        public int Dislike { get; set; }
 
         public string CreatedOn { get; set; }
 
