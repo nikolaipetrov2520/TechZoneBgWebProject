d477e260931fe9ff82c07f0e106e15fa8c5b0c32
nikolaipetrov2520 <64483062+nikolaipetrov2520@users.noreply.github.com>
2022-06-13 19:02:07 +0300

    edit Device Service

--- Services/TechZoneBgWebProject.Services/Devices/DevicesService.cs
+++ Services/TechZoneBgWebProject.Services/Devices/DevicesService.cs
@@ -164,6 +164,9 @@
 
         public async Task CreateAsync(DeviceCreateInputModel input)
         {
+            var condition = this.db.Conditions.FirstOrDefaultAsync(c => !c.IsDeleted && c.Name == "Преглежда се").Id;
+            var status = this.db.Status.FirstOrDefaultAsync(c => !c.IsDeleted && c.Name == "За проверка").Id;
+
             var device = new Device
             {
                 CreatedOn = DateTime.Now,
@@ -171,7 +174,31 @@
                 Color = input.Color,
                 Memory = input.Memory,
                 Seller = input.Seller,
+                ConditionId = condition,
+                StatusId = status,
+
             };
+
+            var checkList = new CheckList
+            {
+                Devices = device,
+            };
+
+            var checklistCheck = new List<CheckListsChecks>
+            {
+                new CheckListsChecks{ CheckList = checkList, CheckId = 1},
+                new CheckListsChecks{ CheckList = checkList, CheckId = 2},
+                new CheckListsChecks{ CheckList = checkList, CheckId = 3},
+                new CheckListsChecks{ CheckList = checkList, CheckId = 4},
+                new CheckListsChecks{ CheckList = checkList, CheckId = 5},
+                new CheckListsChecks{ CheckList = checkList, CheckId = 6},
+                new CheckListsChecks{ CheckList = checkList, CheckId = 7},
+                new CheckListsChecks{ CheckList = checkList, CheckId = 8},
+                new CheckListsChecks{ CheckList = checkList, CheckId = 9},
+                new CheckListsChecks{ CheckList = checkList, CheckId = 10},
+                new CheckListsChecks{ CheckList = checkList, CheckId = 11},
+            };
+
             await this.db.Devices.AddAsync(device);
             await this.db.SaveChangesAsync();
         }
